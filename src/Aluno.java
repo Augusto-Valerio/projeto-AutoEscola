67991a8b091184eed173927409789b220822f1cf
Augusto Valerio <augusto.valerio06@gmail.com>
2026-03-21 00:19:51 -0300

    feat: adicionar métodos de agendamento e cancelamento de aulas

--- src/Aluno.java
+++ src/Aluno.java
@@ -5,4 +5,32 @@ public class Aluno {
     int aulasTeoricas;
     int aulasPraticas;
     boolean aprovado;
+
+    void agendarAulaTeorica() {
+        aulasTeoricas++;
+    }
+
+    void agendarAulaPratica() {
+        aulasPraticas++;
+    }
+
+    // Método para colocar quantas aulas foram agendadas e evitar repetição de codigo na Main
+    void agendarAulas(int quantidade) {
+        for (int i = 0; i < quantidade; i++) {
+            agendarAulaPratica();
+            agendarAulaTeorica();
+        }
+    }
+
+    void cancelarAulaTeorica() {
+        if (aulasTeoricas > 0) {
+            aulasTeoricas--;
+        }
+    }
+
+    void cancelarAulaPratica() {
+        if (aulasPraticas > 0) {
+            aulasPraticas--;
+        }
+    }
 }
