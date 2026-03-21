2c9794584b35854f8a39689c2b2bfacf784cbf8d
Jonas Esteves <jonasesteves2006@gmail.com>
2026-03-21 00:35:33 -0300

    feat: criar atributos da AutoEscola

---
+++ src/AutoEscola.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class AutoEscola {
+    private List<Aluno> alunos = new ArrayList<>();
+
+    public void adicionarAluno(Aluno aluno) {
+        alunos.add(aluno);
+    }
+}
